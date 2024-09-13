fddfef9ff84dc13604b00fed696c18f553134d6b
Rascal0902 <geonho050902@gmail.com>
2024-09-13 13:47:45 +0900

    ckpt

--- CNN.py
+++ CNN.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.nn.init as init
 import argparse
 
 import torchvision.datasets as datasets
@@ -15,12 +14,31 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
 
+import yaml
+import os
+from loguru import logger
+
+
+def load_hparams(cfg_path):
+    with open(cfg_path, 'r', encoding='utf-8') as f:
+        hparams = yaml.safe_load(f)
+    return hparams
+
+
+def save_hparams(hparams, log_dir):
+    with open(os.path.join(log_dir, 'config.yaml'), 'w') as f:
+        yaml.dump(hparams, f)
+
+
 batch_size = 100
 learning_rate = 0.0002
 num_epoch = 10
+log_dir = './logs'
 
-mnist_train = datasets.MNIST(root="../Deep_learn/Data/", train=True, transform=transforms.ToTensor(), target_transform=None, download=True)
-mnist_test = datasets.MNIST(root="../Deep_learn/Data/", train=False, transform=transforms.ToTensor(), target_transform=None, download=True)
+mnist_train = datasets.MNIST(root="../Deep_learn/Data/", train=True, transform=transforms.ToTensor(),
+                             target_transform=None, download=True)
+mnist_test = datasets.MNIST(root="../Deep_learn/Data/", train=False, transform=transforms.ToTensor(),
+                            target_transform=None, download=True)
 
 train_loader = DataLoader(mnist_train, batch_size=batch_size, shuffle=True, num_workers=0, drop_last=True)
 test_loader = DataLoader(mnist_test, batch_size=batch_size, shuffle=False, num_workers=0, drop_last=True)
@@ -67,8 +85,9 @@ class CNN(nn.Module):
         # self.fc_layer 정의한 연산 수행
         out = self.fc_layer(out)
         return out
-    
-def save_checkpoint(model, optimizer, epoch, loss, filename='checkpoint.pth'):
+
+
+def save_checkpoint(model, optimizer, epoch, loss, filename='checkpoint.ckpt'):
     checkpoint = {
         'epoch': epoch,
         'model_state_dict': model.state_dict(),
@@ -78,7 +97,8 @@ def save_checkpoint(model, optimizer, epoch, loss, filename='checkpoint.pth'):
     torch.save(checkpoint, filename)
     print(f'Checkpoint saved to {filename}')
 
-def load_checkpoint(model, optimizer, filename='checkpoint.pth'):
+
+def load_checkpoint(model, optimizer, filename='checkpoint.ckpt'):
     checkpoint = torch.load(filename)
     model.load_state_dict(checkpoint['model_state_dict'])
     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
@@ -87,9 +107,17 @@ def load_checkpoint(model, optimizer, filename='checkpoint.pth'):
     print(f'Checkpoint loaded from {filename}')
     return start_epoch, loss
 
+
 parser = argparse.ArgumentParser(description='PyTorch MNIST Training')
 parser.add_argument('--resume', type=str, default=None, help='path to checkpoint to resume training')
+parser.add_argument('--cfg', type=str, default='config.yaml', help='path to configuration file')
 args = parser.parse_args()
+
+hparams = load_hparams(args.cfg)
+batch_size = hparams.get('batch_size', 100)
+learning_rate = hparams.get('learning_rate', 0.0002)
+num_epoch = hparams.get('num_epoch', 10)
+log_dir = hparams.get('log_dir', './logs')
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 model = CNN().to(device)
@@ -100,7 +128,6 @@ scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
 start_epoch = 0
 if args.resume:
     start_epoch, _ = load_checkpoint(model, optimizer, args.resume)
-
 
 loss_arr = []
 loss_func = nn.CrossEntropyLoss()
@@ -119,8 +146,11 @@ for epoch in range(start_epoch, num_epoch):
         optimizer.step()
 
         if j % 1000 == 0:
-            print(f"Epoch [{epoch+1}/{num_epoch}], Step [{j}/{len(train_loader)}], Loss: {loss.item()}")
+            print(f"Epoch [{epoch + 1}/{num_epoch}], Step [{j}/{len(train_loader)}], Loss: {loss.item()}")
+            logger.info(f"Epoch [{epoch + 1}/{num_epoch}], Step [{j}/{len(train_loader)}], Loss: {loss.item()}")
             loss_arr.append(loss.cpu().detach().numpy())
+
+    save_checkpoint(model, optimizer, epoch, loss.item())
 
 correct = 0
 total = 0
@@ -145,13 +175,15 @@ with torch.no_grad():
 
         # 도출한 모델의 index와 라벨이 일치하면 correct에 개수 추가
         correct += (output_index == y).sum().float()
-        
+
         y_true.extend(y.cpu().numpy())
         y_pred.extend(output_index.cpu().numpy())
 
+    logger.info("Accuracy of Test Data: {}%".format(100 * correct / total))
+
     # 정확도 도출
     print("Accuracy of Test Data: {}%".format(100 * correct / total))
-    
+
 # 혼동 행렬 시각화
 cm = confusion_matrix(y_true, y_pred)
 plt.figure(figsize=(10, 7))
@@ -160,3 +192,5 @@ plt.xlabel('Predicted')
 plt.ylabel('True')
 plt.title('Confusion Matrix')
 plt.show()
+
+save_hparams(hparams, log_dir)
